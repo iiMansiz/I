151c58e4b060f7a4e2ae3f32768ba475133cc902
iMansiz <132319106+iiMansiz@users.noreply.github.com>
2025-05-01 11:10:55 +0700

    Update rajaongkir.js

--- utils/rajaongkir.js
+++ utils/rajaongkir.js
@@ -1,20 +1,19 @@
-// utils/rajaongkir.js
 import axios from "axios";
-
-const API_KEY = "API_KEY_KAMU";
-const BASE_URL = "https://api.rajaongkir.com/starter"; // atau 'pro' jika kamu pakai Pro
+const API_KEY = "RAJAONGKIR_API_KEY";
+const BASE_URL = "https://api.rajaongkir.com/starter";
 
 export const getOngkir = async (origin, destination, weight, courier) => {
-  const res = await axios.post(`${BASE_URL}/cost`, {
-    origin,
-    destination,
-    weight,
-    courier,
-  }, {
+  const form = new URLSearchParams();
+  form.append("origin", origin);
+  form.append("destination", destination);
+  form.append("weight", weight);
+  form.append("courier", courier);
+
+  const res = await axios.post(`${BASE_URL}/cost`, form, {
     headers: {
       key: API_KEY,
-      "content-type": "application/x-www-form-urlencoded"
-    }
+      "content-type": "application/x-www-form-urlencoded",
+    },
   });
 
   return res.data.rajaongkir.results[0].costs;
