2bfeaddd7044617d05f375de7b278734e16deb41
iMansiz <132319106+iiMansiz@users.noreply.github.com>
2025-05-02 21:34:41 +0700

    Delete src directory

--- src/seller/App.jsx
+++
@@ -1,7 +0,0 @@
-import LoginCabang from "./seller/LoginCabang";
-import SellerDashboard from "./seller/SellerDashboard";
-
-const [user, setUser] = useState(null);
-
-<Route path="/seller/login" element={<LoginCabang onLogin={setUser} />} />
-{user && <Route path="/seller/dashboard" element={<SellerDashboard user={user} />} />}
